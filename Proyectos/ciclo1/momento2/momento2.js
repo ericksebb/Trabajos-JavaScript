33a3ead0294f487d6202f16b3d9a60aef66eb053
ericsebb <esprado@sudamericano.edu.ec>
2023-11-30 22:29:06 -0500

    Release 1.02

--- Proyectos/ciclo1/momento2/momento2.js
+++ Proyectos/ciclo1/momento2/momento2.js
@@ -13,8 +13,12 @@ if(player1.nombre == ( null || "")){
 }
 
 let labelP1 = document.getElementById("nick-player-1");
-labelP1.innerHTML = `Nick player 1: ${player1.nombre}`
+labelP1.innerHTML = `Player 1: ${player1.nombre}`
+let livesP1 = document.getElementById("lives-player-1")
+livesP1.innerHTML = `Vidas restantes: ${player1.vidas}`
 
 let player2 = new Player("IA", 3, 5)
 let labelP2 = document.getElementById("nick-player-2")
-labelP2.innerHTML = `Nick player 2: ${player2.nombre}`
+labelP2.innerHTML = `Player 2: ${player2.nombre}`
+let livesP2 = document.getElementById("lives-player-2")
+livesP2.innerHTML = `Vidas restantes: ${player2.vidas}`
