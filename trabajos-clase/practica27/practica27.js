42e7aaf832b19aedc3ea0c239645f8141b65feb3
ericsebb <esprado@sudamericano.edu.ec>
2023-12-11 07:54:15 -0500

    Practica 27

--- trabajos-clase/practica27/practica27.js
+++ trabajos-clase/practica27/practica27.js
@@ -5,7 +5,9 @@ let message = "ecuatoriano"
 //alert(message[1])
 //alert(message.at(1))
 
-
+let cont = 0;
 for(let i = 0; i < message.length; i++){
-    alert(message[i])
+    if(message[i] == "a"){
+        cont ++;
+    } alert(message[i])
 }
