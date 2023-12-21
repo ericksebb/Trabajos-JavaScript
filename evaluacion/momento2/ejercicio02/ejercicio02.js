329475cc311e778f4a7a6df0638e5a786fe404c2
ericsebb <esprado@sudamericano.edu.ec>
2023-12-21 10:21:42 -0500

    Evaluación momento 2

--- evaluacion/momento2/ejercicio02/ejercicio02.js
+++ evaluacion/momento2/ejercicio02/ejercicio02.js
@@ -4,7 +4,12 @@ A a partir del array ages=[15, 50, 45] agregue su edad. Usando un for con un if,
 */
 
 let ages = [15, 50, 45];
-ages.push(18);
+ages.splice(1, 0, 18);
+
+// Los siguientes operadores sirven para colocar el número 45 en orden respectivo 
+ages.pop();
+ages.splice(2, 0, 45);
+
 
 for(let i = 0; i < ages.length; i++){
     if(ages[i] > 25){
