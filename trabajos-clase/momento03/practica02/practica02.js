abac540de24bc3c407a5d2d39260150e1bac021d
ericsebb <esprado@sudamericano.edu.ec>
2024-01-18 10:50:49 -0500

    Hotfix

--- trabajos-clase/momento03/practica02/practica02.js
+++ trabajos-clase/momento03/practica02/practica02.js
@@ -79,4 +79,4 @@ alert(`${programmingBooks.find(element => element == book) ? "Si" : "No"} se enc
 dentro de una biblioteca, el índice del vector indica el numero de la estantería.
 Mostrar el numero de la estantería donde se encuentra el libro “You Don’t Know JS” */
 
-alert(`El libro "You Don’t Know JS" se encuentra en la estanteria ${programmingBooks.indexOf("You Don't Know JS")}`);
+alert(`El libro "You Don't Know JS" se encuentra en la estanteria ${programmingBooks.indexOf("You Don't Know JS")}`);
